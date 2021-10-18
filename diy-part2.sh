6d01a5f12d9069d07fb81fa0a8bea5adb9f8b45a
MonwF <boluo2@gmail.com>
2021-10-18 13:24:18 +0800

    fix: 修改lan时，无线会禁用

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,10 +13,10 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's#<%=luci.sys.exec("cat /etc/bench.log") or " "%>##' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-sed -i "/luci.model.network/a local wifi_infs = \"ra0 rai0\"" iface.htm
-sed -i '/"_custom")/a attr("checked", "checked") ..' iface.htm
-sed -i 's#attr("value", " ")#attr("value", wifi_infs)#' iface.htm
-sed -i "s/_custom').checked=true/_custom').checked=true\" value=\"<%=wifi_infs%>/" iface.htm
+sed -i "/luci.model.network/a local wifi_infs = \"ra0 rai0\"" feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
+sed -i '/"_custom")/a attr("checked", "checked") ..' feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
+sed -i 's#attr("value", " ")#attr("value", wifi_infs)#' feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
+sed -i "s/_custom').checked=true/_custom').checked=true\" value=\"<%=wifi_infs%>/" feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
 sed -i '/ppp-mod-pppoe/,/ddns-scripts_dnspod/{//!d;};' include/target.mk
 sed -i '/ddns-scripts_dnspod/d' include/target.mk
 sed -i '/ppp-mod-pppoe/a luci-newapi kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw luci luci-app-wol curl' include/target.mk
