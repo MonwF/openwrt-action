10d1b01456d6b09d9902ae191cab5fa574deb417
MonwF <boluo2@gmail.com>
2021-10-18 10:04:31 +0800

    移除bench.log

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's#<%=luci.sys.exec("cat /etc/bench.log") or " "%>##' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 sed -i '/ppp-mod-pppoe/,/ddns-scripts_dnspod/{//!d;};' include/target.mk
 sed -i '/ddns-scripts_dnspod/d' include/target.mk
 sed -i '/ppp-mod-pppoe/a luci-newapi kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw luci luci-app-wol curl' include/target.mk
