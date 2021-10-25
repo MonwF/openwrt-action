a6bdc772636cf57537e4531e6d3884ce1c9bfdd6
MonwF <boluo2@gmail.com>
2021-10-25 12:24:57 +0800

    部分修改调整至首次启动时执行

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,13 +11,14 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's#<%=luci.sys.exec("cat /etc/bench.log") or " "%>##' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-sed -i "/luci.model.network/a local wifi_infs = \"ra0 rai0\"" feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
-sed -i '/"id".*"_custom")/a ifattr(self.network == "lan", "checked", "checked") ..' feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
-sed -i 's#attr("value", " ")#ifattr(self.network == "lan", "value", wifi_infs)#' feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
-sed -i "s/_custom').checked=true\"/_custom').checked=true\" <%=ifattr(self.network == \"lan\", \"value\", wifi_infs)%>/" feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# sed -i 's#<%=luci.sys.exec("cat /etc/bench.log") or " "%>##' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+# sed -i "/luci.model.network/a local wifi_infs = \"ra0 rai0\"" feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
+# sed -i '/"id".*"_custom")/a ifattr(self.network == "lan", "checked", "checked") ..' feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
+# sed -i 's#attr("value", " ")#ifattr(self.network == "lan", "value", wifi_infs)#' feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
+# sed -i "s/_custom').checked=true\"/_custom').checked=true\" <%=ifattr(self.network == \"lan\", \"value\", wifi_infs)%>/" feeds/luci/modules/luci-base/luasrc/view/cbi/network_ifacelist.htm
+# sed -i 's/START=95/START=15/g' package/base-files/files/etc/init.d/done
+
 sed -i '/ppp-mod-pppoe/,/ddns-scripts_dnspod/{//!d;};' include/target.mk
 sed -i '/ddns-scripts_dnspod/d' include/target.mk
 sed -i '/ppp-mod-pppoe/a luci-newapi kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw luci luci-app-wol curl' include/target.mk
-sed -i 's/START=95/START=15/g' package/base-files/files/etc/init.d/done
